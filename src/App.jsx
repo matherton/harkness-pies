2e204679a7e5222605362737a53bf83c0bdd56af
Mark Atherton <mark@insightinternet.co.uk>
2024-03-08 14:44:17 +0000

    <Home /> and included it in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import Home from "./components/Home";
 import People from "./components/People";
 import HockeyTeamList from "./components/HockyTeam";
 import Footer from "./components/Footer";
@@ -10,25 +11,8 @@ function App() {
   return (
     <>
       <Header />
-      <div className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12 px-4 sm:px-1 lg:px-8">
-        <div className="grid grid-cols-2 gap-4 pt-10 pl-10 pr-10">
-          <div>
-            <People />
-          </div>
-          <div>02</div>
-          <div>03</div>
-          <div>
-            <ToggleNav />
-          </div>
-          <div>05</div>
-          <div>06</div>
-          <div>07</div>
-          <div>
-            <HockeyTeamList />
-          </div>
-          <div>09</div>
-        </div>
-      </div>
+      <Home />
+
       <Footer />
     </>
   );
