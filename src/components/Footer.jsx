017f858a35ee28674b63bae60910efce734570a8
Mark Atherton <mark@insightinternet.co.uk>
2024-03-09 16:56:26 +0000

    made up <Footer /> content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-100 grid grid-cols-1 gap-4 text-center">
-      30b Raeburn Place
-      <br /> Edinburgh <br />
-      EH4 1HN
+    <footer className="bg-slate-600 text-white grid grid-cols-1 p-4 border-b-2 border-t-2 text-center">
+      30b Raeburn Place Edinburgh EH4 1HN
       <br />
       Mon-Sat 10-5, Sun 11-3
-      <a className="pt-0" href="mailto:hello@harknesspies.co.uk">
+      <a
+        className="pt-0 no-underline hover:underline"
+        href="mailto:hello@harknesspies.co.uk"
+      >
         hello@harknesspies.co.uk
       </a>
     </footer>
