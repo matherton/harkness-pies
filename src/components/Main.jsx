7aea38165280d0766b926aae85ae1fa77c906137
Mark Atherton <mark@insightinternet.co.uk>
2024-03-11 12:31:43 +0000

    inserted link to vite.ico and made col span 2 only above sm screen width

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ function Main(props) {
   return (
     <>
       <div className="grid-rows-reverse relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12 px-4 sm:px-1 lg:px-8">
-        <div className="grid grid-cols-2 gap-4 pt-10 pl-10 pr-10 text-center ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 pt-10 pl-10 pr-10 text-center ">
           {props.reverse ? (
             <>
               <div>
